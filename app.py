6e18bbf854d92a3d77183d9419d1f0373df9a97a
GrolarKim <audwnswmf@naver.com>
2021-12-29 19:10:35 +0900

    render_templates

--- app.py
+++ app.py
@@ -3,12 +3,29 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
-def showIndex():
+def showIndexPage():
     return render_template('index.html')
 
 @app.route('/profile')
-def showProfile():
+def showProfilePage():
     return render_template('profile.html')
+
+@app.route('/login')
+def showLoginPage():
+    return render_template('login.html')
+
+@app.route('/register')
+def showRegisterPage():
+    return render_template('register.html')
+
+@app.route('/posting')
+def showPostingPage():
+    return render_template('posting.html')
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
