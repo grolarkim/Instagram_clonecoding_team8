8b99116bd3f78bc9acff18cfa1b06fb7fe0afb08
GrolarKim <audwnswmf@naver.com>
2021-12-29 16:32:01 +0900

    add_profile

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def showIndex():
     return render_template('index.html')
 
+@app.route('/profile')
+def showProfile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
