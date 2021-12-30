5f810f66befab7d321f1cf3838146367e8170558
GrolarKim <audwnswmf@naver.com>
2021-12-30 10:51:20 +0900

    loginapi1

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ db = client.dbsparta
 
 import hashlib
 
+import jwt
+SECRET_KEY = 'SPARTA'
+
+import datetime
 
 @app.route('/')
 def showIndexPage():
@@ -37,9 +41,34 @@ def showTimeLine():
 def showMyPost():
     return 
 
-@app.route('/api/login')
+@app.route('/api/login', methods=['POST'])
 def Login():
-    return 
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.register.find_one({'id': id_receive, 'pw': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=암호화 풀기)해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
@@ -61,12 +90,18 @@ def api_register():
     db.register.insert_one(doc)
     return jsonify({'msg': '가입완료'}) 
 
-@app.route('/api/posting')
+@app.route('/api/posting', methods=['POST'])
 def Posting():
-    return 
-
-
-
+    id_receive = request.form['id_give']
+    url_receive = request.form['url_give']
+    desc_receive = request.form['desc_give']
+    doc = {
+        'id': id_receive,
+        'url': url_receive,
+        'desc': desc_receive
+    }
+    db.post.insert_one(doc)
+    return jsonify({'msg': '게시물 생성 완료!'}) 
 
 
 
