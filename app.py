ee3684b727adb3bd755e73efbcdb84f0c7506aab
gaeul <e0540@hanmail.net>
2021-12-30 10:38:06 +0900

    api/login을 위해 수정

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-import hashlib
+import hashlib #pw해시함수화 위해 임포트
+from datetime import datetime,timedelta #로그인 유지를 위해 필요하니 임포트
+import jwt #얘를 임포트하기위해 인터프리터에 PyJwt패키지 설치!
 
+SECRET_KEY = 'sparta'
 
 @app.route('/')
 def showIndexPage():
@@ -37,11 +40,26 @@ def showTimeLine():
 def showMyPost():
     return
 
-@app.route('/api/login')
+@app.route('/api/login')#로그인
 def Login():
-    return
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    #받아온 패스워드를 해시함수화하기
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    #DB에서 post로 받아온 id와, pw에 맞는값 불러와서 result에 저장
+    result = db.register.find_one({'id': id_receive, 'password': pw_hash})
 
+    if result is not None:
+        payload = {
+            'id': id_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60)  # 로그인 1분 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result':'fail', 'msg' : '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
